e6d6348883dc04a9d6f1a47931070ccaf1dca40b
Renan Siqueira <renan.siqueira@riachuelo.com.br>
2023-03-08 10:00:59 -0300

    primeiro commit

---
+++ projeto/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Configurar body-parser para lidar com JSON
+app.use(bodyParser.json());
+
+// Criar conexão com o banco de dados MySQL
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'sua_senha_do_mysql',
+  database: 'seu_banco_de_dados'
+});
+
+// Conectar ao banco de dados MySQL
+connection.connect((error) => {
+  if (error) {
+    console.log('Erro ao conectar ao banco de dados: ' + error);
+  } else {
+    console.log('Conexão bem sucedida ao banco de dados!');
+  }
+});
+
+// Rota para listar todos os usuários
+app.get('/people', (req, res) => {
+  const query = 'SELECT * FROM people';
+  connection.query(query, (error, results) => {
+    if (error) {
+      console.log('Erro ao executar a consulta: ' + error);
+      res.sendStatus(500);
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+// Rota para criar um novo usuário
+app.post('/people', (req, res) => {
+  const query = 'INSERT INTO people (id, name) VALUES (?, ?)';
+  const params = [req.body.name, req.body.email];
+  connection.query(query, params, (error, result) => {
+    if (error) {
+      console.log('Erro ao executar a consulta: ' + error);
+      res.sendStatus(500);
+    } else {
+      res.json({ message: 'Usuário criado com sucesso!' });
+    }
+  });
+});
+
+// Iniciar o servidor
+app.listen(3000, () => {
+  console.log('Servidor iniciado na porta 3000!');
+});
